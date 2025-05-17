257acdb35932de2b6f347bb65c1d09144b963d47
Louka3 <lkuczykowski@gmail.com>
2025-05-17 01:02:03 -0400

    utilizing listdir() from os library to view the contents of Downloads directory. currently hardcoded path but should make it so user has to input path for modularity

--- file_organizer/main.py
+++ file_organizer/main.py
@@ -1,1 +1,8 @@
-print("hello")
+import os
+
+# The os.listdir() function takes a path as an argument and returns a 
+# list of strings representing the names of all entries in the directory, 
+# including both files and subdirectories.
+
+downloads = os.listdir("/home/lou/Downloads")
+print(downloads) 
